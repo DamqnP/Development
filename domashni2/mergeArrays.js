17f3aec37e30766e32739dd69d434778fbb3eee9
Damqn Pavlov <djamiang@abv.bg>
2024-02-14 12:12:33 +0200

    nestFunc

---
+++ domashni2/mergeArrays.js
@@ -0,0 +1,12 @@
+function mergeArrays(arr1,arr2){
+
+    
+    for(let i =0;i<=arr1.length;i++){
+      if(i%2===0){
+        let ourNum=arr1[i];
+      }
+    }
+
+}
+mergeArrays(['5', '15', '23', '56', '35'],
+['17', '22', '87', '36', '11'])
